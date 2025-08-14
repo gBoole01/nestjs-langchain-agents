a5e2b169376ce0723643c57e57b133e9c94a1643
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2025-08-15 01:48:06 +0200

    ci: prepare for production

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import express from 'express';
 import { AppModule } from './app.module';
 
+let cachedServer: any;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const expressApp = express();
+  const server = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+  await server.init();
+  return expressApp;
 }
-bootstrap();
+
+export default async (req, res) => {
+  if (!cachedServer) {
+    cachedServer = await bootstrap();
+  }
+  cachedServer(req, res);
+};

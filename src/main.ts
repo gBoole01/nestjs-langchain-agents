13498e3e26831d8ec9dffcb0d3f3eb6ba4b03bfa
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2025-08-29 21:45:23 +0200

    feat: production ready

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 import { AppModule } from './app.module';
 
-async function bootstrapProd() {
+let cachedServer: any;
+
+async function bootstrap() {
   const expressApp = express();
   const server = await NestFactory.create(
     AppModule,
@@ -13,20 +24,9 @@ async function bootstrapProd() {
   return expressApp;
 }
 
-async function bootstrapDev() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
-}
-
-if (process.env.NODE_ENV === 'production') {
-  let cachedServer: any;
-  const serverlessHandler = async (req, res) => {
-    if (!cachedServer) {
-      cachedServer = await bootstrapProd();
-    }
-    cachedServer(req, res);
-  };
-  module.exports = serverlessHandler;
-} else {
-  bootstrapDev();
-}
+export default async (req, res) => {
+  if (!cachedServer) {
+    cachedServer = await bootstrap();
+  }
+  cachedServer(req, res);
+};
